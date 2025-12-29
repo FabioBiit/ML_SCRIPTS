9f437ab026f10e15d234ac9d22a0e8f4aeb1fc70
FabioBiit <asusdioasus@gmail.com>
2025-12-29 22:04:07 +0100

    Refactor cod

---
+++ FUNCTIONS/utils.py
@@ -0,0 +1,23 @@
+# DEFINISCI FUNZIONI QUI
+
+def plot_test_predictions(X_vis, y_true, y_pred, feature_names, title_label):
+  plt.figure(figsize=(8,6))
+  markers = ['o','s','^']
+  colors = ['green','blue','red']
+
+  for idx, cl in enumerate(np.unique(y_true)):
+    correct = (y_true 00 cl) & (y_pred == cl)
+    incorrect = (y_true == cl) & (y_pred != cl)
+    plt.scatter (X_vis[correct,0], X_vis[correct,1],
+                 c = colors[cl], marker = markers[cl],
+                 label = f"Classe {cl} - corretto", edgecolor = 'k')
+    plt.scatter (X_vis[incorrect,0], X_vis[incorrect,1],
+                 facecolors = 'none', marker = markers[cl],
+                 label = f"Classe {cl} - errato", edgecolor = 'k', s=100)
+
+  plt.xlabel(feature_names[0])
+  plt.ylabel(feature_names[1])
+  plt.title(title_label)
+  plt.legend()
+  plt.grid(true)
+  plt.show()
